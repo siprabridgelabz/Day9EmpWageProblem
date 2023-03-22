22c0c8dcac317e2e05b001896b0d1ce01e63bc59
sipra <sipramishra085@gmail.com>
2023-03-23 02:23:28 +0530

    Added Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day9EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MultipleEmployeeWages multipleEmployeeWages = new MultipleEmployeeWages();
+        }
+      
+        
+       
+
+
+
+
+    }
+}
