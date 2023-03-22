13d8e39044b89f8035d3c90bb1b0e08c58a7fdbc
siprabridgelabz <122585011+siprabridgelabz@users.noreply.github.com>
2023-03-23 02:52:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         public static void Main(string[] args)
         {
-            MultipleEmployeeWages multipleEmployeeWages = new MultipleEmployeeWages();
+
+
+            Console.WriteLine("Welcome to employee wage computation problem");
+
+
+
+            // EmployeeAttendence attendence = new EmployeeAttendence();
+
+            // EmployeeAttendence.EmpAttendance();
+
+            //EmployeeCheck.EmployeeWage();
+            //EmployeeCheck check = new EmployeeCheck();
+
+            // PartTimeEmployee.PartTimeEmployeeWage();
+            // PartTimeEmployee employee = new PartTimeEmployee();
+
+            // EmployeeWageSwitcCase.EmployeeWageSwitchCaseStatement();
+            // EmployeeWageSwitcCase switcCase = new EmployeeWageSwitcCase();
+
+            // WagesForMonth.WagesforaMonth();
+            // WagesForMonth month = new WagesForMonth();
+
+
+            // EmployeeWageCondition.EmployeeWageConditionForWorkingHours();
+            //  EmployeeWageCondition condition = new EmployeeWageCondition();
+
+          
+
+            //MultipleEmployeeWages multipleEmployeeWages = new MultipleEmployeeWages();
+
+            //ComputeEmployeeWage computeEmployeeWage = new ComputeEmployeeWage();
+
+
+
+
+
         }
       
         
